1ebc9ceb2822846554b34fadf0983c1e96870fc8
bouncyblock <99731358+bouncyblock@users.noreply.github.com>
2024-04-08 16:50:24 -0400

    Add files via upload
    v2

--- bouncy_blockssite.lua
+++ bouncy_blockssite.lua
@@ -1,4 +1,4 @@
---[[pod_format="raw",created="2024-04-08 13:52:56",modified="2024-04-08 13:52:56",revision=0]]
+--[[pod_format="raw",created="2024-04-08 13:52:56",modified="2024-04-08 20:48:20",revision=1]]
 --[[	Thanks for using PicoNet Sitebuilder! I look forward
 	to seeing your picosite! In order to make this available
 	on PicoNet Explorer, you'll need to upload this lua file
@@ -13,29 +13,36 @@
 ]]
 
 	p = {
-	title="Bouncy_Block ",
+	title="untitled_page ",
 	g=create_gui({
 		x=0,y=0,
-		width=200,height=200,
+		width=300,height=300,
 		fgcol = 0x090d
 	}),
 	copybuttons = {},
 	init = function(self,explorer)
 		local page = self
-		add(self.copybuttons,self.g:attach({
-			x=86.0, y=13.0, width=16, height=16,
+		self.button9 = self.g:attach_button({
+			x=219.0, y=93.5, width=70, height=13,
+			label="Join the jam! ",
+			click=function()
+				explorer:open_page("https://itch.io/jam/picotron-jam")
+			end
+		})
+		add(self.copybuttons, self.g:attach({
+			x=198.0, y=80.0, width=15, height=15,
 			event = function(self,msg)
 				if(msg.event == "release") then
-					set_clipboard(pod({type="gif", w=16, h=16, frames=12, speed=1, clr=0, imgdata=page.gif3_gif_raw}))
-					notify("gif added to clipboard")
+					set_clipboard("--[[pod,pod_type=\"image\"]]unpod(\""..page.image10_raw.."\")")
+					notify("image userdata added to clipboard")
 				end
 			end
 		}))
 		add(self.copybuttons,self.g:attach({
-			x=86.0, y=109.0, width=16, height=16,
+			x=57.0, y=6.0, width=16, height=16,
 			event = function(self,msg)
 				if(msg.event == "release") then
-					set_clipboard(pod({type="gif", w=16, h=16, frames=12, speed=1, clr=0, imgdata=page.gif3_gif_raw}))
+					set_clipboard(pod({type="gif", w=16, h=16, frames=12, speed=1, clr=0, imgdata=page.gif11_gif_raw}))
 					notify("gif added to clipboard")
 				end
 			end
@@ -46,14 +53,24 @@
 	end,
 	draw = function(self,explorer)
 		cls(0)
-		print("This is where I\nwill post things\nI've\nbeen doing, for\nexample, I am\ncurently running\nthe\nPICOTRON\nJAM!\nhttps://www.lex\naloffle.com/bbs/\n?tid=141532\n ",108.0,10.0,7)
-		self.gif3:draw(86.0,13.0,16,16,1,0)
-		self.gif3:draw(86.0,109.0,16,16,1,0)
+		rectfill(0,-328,300,172,26)
+		ovalfill(10.0,10.0,60.0,60.0,12)
+		print("Bouncy's\nSite\n ",15.0,24.0,7)
+		print("Welcome to my site, the place where\nroads meet!\nHere you can find whatever I'm working\non, challenges, game jams, art, or just\nrandom stuff!\n\n ",75.0,9.0,8)
+		rectfill(13.0,79.0,213.0,119.0,9)
+		print("PICOTRON JAM!\nThis is the game jam that I'm running.\nStatus: Not started ",13.0,81.0,7)
+		palt(0)
+		palt(0, true)
+		sspr(self.image10, 0, 0, 8,8,198.0,80.0,15,15)
+		palt()
+		self.gif11:draw(57.0,6.0,16,16,1,0)
 		
 	end,
 	update = function(self,explorer)
 		
 	end,
-	gif3_gif_raw = "b64:bHo0ALcBAAAQAwAA-AJweHUAQyDAEATwtD8WsE7ALgIA-wCgTpAOLw8ekA4tLqAOHR4EAA3-BYAOLS5wLi0OkB4tHpAeLQ4NgC4dBQAOACgA-wVwHi0ecH6AfoBugA4fIA4cDoAOHAYAD7ducH5gBR4cDhyAHhYADwYAHBFwPACAcAUOHA0MDYAHAAUgAD8uDC4EAAYVDjYAAAcAgS4MDqAuDB6QHgAfCQQACAE_AP8EHpAFLgmgBR4JDrAeCQ4FoB4JCgUADzEOBZAyAP8EFQ0ACQqQFQ0JCrAFDQrABQ0ZsAQAByAKsCkA8gTgDxkZCqALDQkNoA0ZsA0OGgmgBQBGCgkPFAwAFAgLAHAOGgmwDRnQMQDxBtANCQ0JGoAdGRpgDQ4JKpANChgJoAUAWwoJKAmQCgACBQDyEw4JKsAdGRqgDg0JCgQJgA4ZCgQJYA0OCggECZANCQgECaAGAG4KGAQLCZAMAAMGAAExABHAPgCCoA4LCgQLCoAHALBgDQAJBAsKkA0psAMAWQkICwmgCQAiKbAiABLAMAD1BqANACmQDQApcA0AKaAN4A3gDQAZsAYAwOANACnQDQApsA3wpQ==",
-	gif3 = new_gif("b64:bHo0ALcBAAAQAwAA-AJweHUAQyDAEATwtD8WsE7ALgIA-wCgTpAOLw8ekA4tLqAOHR4EAA3-BYAOLS5wLi0OkB4tHpAeLQ4NgC4dBQAOACgA-wVwHi0ecH6AfoBugA4fIA4cDoAOHAYAD7ducH5gBR4cDhyAHhYADwYAHBFwPACAcAUOHA0MDYAHAAUgAD8uDC4EAAYVDjYAAAcAgS4MDqAuDB6QHgAfCQQACAE_AP8EHpAFLgmgBR4JDrAeCQ4FoB4JCgUADzEOBZAyAP8EFQ0ACQqQFQ0JCrAFDQrABQ0ZsAQAByAKsCkA8gTgDxkZCqALDQkNoA0ZsA0OGgmgBQBGCgkPFAwAFAgLAHAOGgmwDRnQMQDxBtANCQ0JGoAdGRpgDQ4JKpANChgJoAUAWwoJKAmQCgACBQDyEw4JKsAdGRqgDg0JCgQJgA4ZCgQJYA0OCggECZANCQgECaAGAG4KGAQLCZAMAAMGAAExABHAPgCCoA4LCgQLCoAHALBgDQAJBAsKkA0psAMAWQkICwmgCQAiKbAiABLAMAD1BqANACmQDQApcA0AKaAN4A3gDQAZsAYAwOANACnQDQApsA3wpQ==",12)
+	gif11_gif_raw = "b64:bHo0ALcBAAAQAwAA-AJweHUAQyDAEATwtD8WsE7ALgIA-wCgTpAOLw8ekA4tLqAOHR4EAA3-BYAOLS5wLi0OkB4tHpAeLQ4NgC4dBQAOACgA-wVwHi0ecH6AfoBugA4fIA4cDoAOHAYAD7ducH5gBR4cDhyAHhYADwYAHBFwPACAcAUOHA0MDYAHAAUgAD8uDC4EAAYVDjYAAAcAgS4MDqAuDB6QHgAfCQQACAE_AP8EHpAFLgmgBR4JDrAeCQ4FoB4JCgUADzEOBZAyAP8EFQ0ACQqQFQ0JCrAFDQrABQ0ZsAQAByAKsCkA8gTgDxkZCqALDQkNoA0ZsA0OGgmgBQBGCgkPFAwAFAgLAHAOGgmwDRnQMQDxBtANCQ0JGoAdGRpgDQ4JKpANChgJoAUAWwoJKAmQCgACBQDyEw4JKsAdGRqgDg0JCgQJgA4ZCgQJYA0OCggECZANCQgECaAGAG4KGAQLCZAMAAMGAAExABHAPgCCoA4LCgQLCoAHALBgDQAJBAsKkA0psAMAWQkICwmgCQAiKbAiABLAMAD1BqANACmQDQApcA0AKaAN4A3gDQAZsAYAwOANACnQDQApsA3wpQ==",
+	image10_raw = "b64:bHo0ADMAAAAxAAAA8CJweHUAQyAICAQQPxQwBR4FIAUIDxgIDQUQBTgFEAUNCA0IBRAFKA0FEAUIDRgFIDUQ",
+	image10 = unpod("b64:bHo0ADMAAAAxAAAA8CJweHUAQyAICAQQPxQwBR4FIAUIDxgIDQUQBTgFEAUNCA0IBRAFKA0FEAUIDRgFIDUQ"),
+	gif11 = new_gif("b64:bHo0ALcBAAAQAwAA-AJweHUAQyDAEATwtD8WsE7ALgIA-wCgTpAOLw8ekA4tLqAOHR4EAA3-BYAOLS5wLi0OkB4tHpAeLQ4NgC4dBQAOACgA-wVwHi0ecH6AfoBugA4fIA4cDoAOHAYAD7ducH5gBR4cDhyAHhYADwYAHBFwPACAcAUOHA0MDYAHAAUgAD8uDC4EAAYVDjYAAAcAgS4MDqAuDB6QHgAfCQQACAE_AP8EHpAFLgmgBR4JDrAeCQ4FoB4JCgUADzEOBZAyAP8EFQ0ACQqQFQ0JCrAFDQrABQ0ZsAQAByAKsCkA8gTgDxkZCqALDQkNoA0ZsA0OGgmgBQBGCgkPFAwAFAgLAHAOGgmwDRnQMQDxBtANCQ0JGoAdGRpgDQ4JKpANChgJoAUAWwoJKAmQCgACBQDyEw4JKsAdGRqgDg0JCgQJgA4ZCgQJYA0OCggECZANCQgECaAGAG4KGAQLCZAMAAMGAAExABHAPgCCoA4LCgQLCoAHALBgDQAJBAsKkA0psAMAWQkICwmgCQAiKbAiABLAMAD1BqANACmQDQApcA0AKaAN4A3gDQAZsAYAwOANACnQDQApsA3wpQ==",12)
 }
